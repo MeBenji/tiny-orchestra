182c241bce3afbed6c318c50fb028183cd04e4f4
Benjamin Caddell <benji@caddell.de>
2026-06-25 23:49:12 +0200

    Update Scoremanager to load and save highscore and be persistent

--- Assets/Scripts/ScoreManager/ScoreManager.cs
+++ Assets/Scripts/ScoreManager/ScoreManager.cs
@@ -3,23 +3,50 @@ using UnityEngine;
 
 public class ScoreManager : MonoBehaviour
 {
+    public static ScoreManager Instance;
+
     public delegate void SetPoints(int delta);
     public static SetPoints AddPoints;
 
     int points;
     [SerializeField] TMP_Text PointText;
 
-    private void Awake() {
+    int highScore;
+
+    private void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+        }
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+
         AddPoints = OnAddPoints;
         UpdateUI();
     }
 
-    void OnAddPoints(int delta) {
+    void OnAddPoints(int delta)
+    {
         points += delta;
         UpdateUI();
     }
 
-    void UpdateUI() {
+    void UpdateUI()
+    {
         PointText.text = points.ToString();
+    }
+
+    void LoadHighScore()
+    {
+        if (PlayerPrefs.HasKey(Constants.HIGHSCORE))
+        {
+            highScore = PlayerPrefs.GetInt(Constants.HIGHSCORE);
+        }
+    }
+
+    void SaveHighScore()
+    {
+        PlayerPrefs.SetInt(Constants.HIGHSCORE, highScore);
     }
 }
