bbd315a4b14888abd295d5920761e7a3e43b6e28
Benjamin Caddell <benji@caddell.de>
2026-06-26 12:00:46 +0200

    update direct script

--- Assets/Scripts/DirectionSystem/Direct.cs
+++ Assets/Scripts/DirectionSystem/Direct.cs
@@ -8,36 +8,64 @@ public class Direct : MonoBehaviour
     PlayerInput input;
     Camera cam;
 
-    Enemy currentEnemy;
+    EnemyController currentEnemy;
 
     [SerializeField] Material defaultMaterial;
     [SerializeField] Material mouseOnMaterial;
 
-    private void Awake() {
+    [SerializeField] bool mouseAim;
+
+    private void Awake()
+    {
         input = GetComponent<PlayerInput>();
         cam = Camera.main;
     }
 
-    private void Update() {
-        RayCast(Input.mousePosition);
+    private void Update()
+    {
+        if (mouseAim)
+        {
+            RayCastMouse(Input.mousePosition);
+        }
+        else
+        {
+            RayCastPlayer();
+        }
 
-        if(Input.GetMouseButtonDown(0) && currentEnemy != null) {
-            currentEnemy.OnPlayerClicksOn();
+        if (Input.GetMouseButtonDown(0) && currentEnemy != null)
+        {
+            // currentEnemy.OnPlayerClicksOn();
+            currentEnemy.onPlayInstrument?.Invoke();
         }
     }
 
-    void RayCast(Vector3 mousePos) {
-        Ray ray = cam.ScreenPointToRay(mousePos);
-
-        Debug.DrawRay(ray.origin, ray.direction * 100);
-        if(Physics.Raycast(ray, out RaycastHit hit, Mathf.Infinity, enemyMask)) {
+    void RayCast(Ray ray)
+    {
+        float maxDist = 25f;
+        Debug.DrawRay(ray.origin, ray.direction * maxDist);
+        if (Physics.Raycast(ray, out RaycastHit hit, maxDist, enemyMask))
+        {
             Debug.DrawLine(ray.origin, hit.point, Color.red);
 
-            currentEnemy = hit.transform.GetComponent<Enemy>();
+            currentEnemy = hit.transform.GetComponent<EnemyController>();
             currentEnemy.getRenderer().sharedMaterial = mouseOnMaterial;
 
-        } else if(currentEnemy != null) {
+        }
+        else if (currentEnemy != null)
+        {
             currentEnemy.getRenderer().sharedMaterial = defaultMaterial;
         }
+    }
+
+    void RayCastMouse(Vector3 mousePos)
+    {
+        Ray ray = cam.ScreenPointToRay(mousePos);
+        RayCast(ray);
+    }
+
+    void RayCastPlayer()
+    {
+        Ray ray = new Ray(transform.position, transform.forward);
+        RayCast(ray);
     }
 }
