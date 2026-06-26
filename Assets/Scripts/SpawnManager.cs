0696740c10a785c3b84a12be9d5e87759b483b9b
Benjamin Caddell <benji@caddell.de>
2026-06-26 16:29:04 +0200

    add enemy spawning

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -1,38 +1,108 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.InputSystem;
 
 public class SpawnManager : MonoBehaviour
 {
-    [SerializeField] InputAction fire;
-    [SerializeField] float radius;
-    [SerializeField] ObjectPooler enemyPooler;
+    [System.Serializable]
+    public struct Phase
+    {
+        public GameObject[] prefabs;
+        public int quantity;
+        public bool minOneEach;
+    }
+
+    [SerializeField] float spawnRadius;
+    public float spawnDelay;
+    [SerializeField] List<Phase> phases;
+    [SerializeField] List<GameObject> enemySpawnList;
 
     private void Awake()
     {
-        fire.performed += (_) => { SpawnEnemy(); };
-        fire.Enable();
+        BuildEnemySpawnList();
+    }
+
+    private void Start()
+    {
+        StartCoroutine(SpawnRoutine());
+    }
+
+    private IEnumerator SpawnRoutine()
+    {
+        foreach(GameObject enemy in enemySpawnList)
+        {
+            SpawnEnemy(enemy);
+            yield return new WaitForSeconds(spawnDelay);
+        }
+    }
+
+    private GameObject GetRandomFromArray(GameObject[] prefabs)
+    {
+        int index = Random.Range(0, prefabs.Length);
+        return prefabs[index];
+    }
+
+    private List<T> Shuffle<T>(List<T> list)
+    {
+        int length = list.Count;
+        List<T> tmpList = new List<T>();
+        for (int i = 0; i < length; i++)
+        {
+            int index = Random.Range(0, list.Count);
+            tmpList.Add(list[index]);
+            list.RemoveAt(index);
+        }
+        return tmpList;
+    }
+
+    private void BuildEnemySpawnList()
+    {
+        foreach (Phase phase in phases)
+        {
+            int remaining = phase.quantity;
+            List<GameObject> phaseSpawnList = new List<GameObject>();
+            if (phase.minOneEach)
+            {
+                for (int i = 0; i < phase.prefabs.Length; i++)
+                {
+                    phaseSpawnList.Add(phase.prefabs[i]);
+                }
+                remaining -= phase.prefabs.Length;
+            }
+            for (int i = 0; i < remaining; i++)
+            {
+                phaseSpawnList.Add(GetRandomFromArray(phase.prefabs));
+            }
+
+            phaseSpawnList = Shuffle(phaseSpawnList);
+
+            foreach(GameObject prefab in phaseSpawnList)
+            {
+                enemySpawnList.Add(prefab);
+            }
+        }
     }
 
     private void OnDrawGizmosSelected()
     {
-        Gizmos.color=Color.yellow;
-        Gizmos.DrawWireSphere(transform.position, radius);
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(transform.position, spawnRadius);
     }
 
     private Vector3 GetSpawnLocation()
     {
-        Vector2 xzPosition = radius * Random.onUnitCircle;
+        Vector2 xzPosition = spawnRadius * Random.onUnitCircle;
         return transform.position + new Vector3(xzPosition.x, 0, xzPosition.y);
     }
 
-    public void SpawnEnemy()
+    private void SpawnEnemy(GameObject enemyPrefab)
     {
-        GameObject enemy = enemyPooler.GetGameObject();
-        if (enemy == null) {
-            Debug.Log("Failed to retrieve enemy from pool.");
-            return;
-        }
-        enemy.transform.position = GetSpawnLocation();
+        Vector3 position = GetSpawnLocation();
+        Vector3 lookDirection = transform.position - position;
+        lookDirection = new Vector3(lookDirection.x, 0f, lookDirection.z).normalized;
+        GameObject enemy = Instantiate(enemyPrefab);
+        enemy.transform.position = position;
+        enemy.transform.forward = lookDirection;
         enemy.SetActive(true);
     }
 }
