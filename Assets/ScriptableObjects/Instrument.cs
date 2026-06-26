ef3b2eb85cf25faa62b22c9016018bbb378ed2ef
Benjamin Caddell <benji@caddell.de>
2026-06-26 11:56:03 +0200

    Added instrument scriptable objects

---
+++ Assets/ScriptableObjects/Instrument.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Tiny Orchestra/Instrument")]
+public class Instrument : ScriptableObject
+{
+    public TracksSystem.Instruments type;
+    public AudioClip track;
+    public AudioClip introSound;
+}
